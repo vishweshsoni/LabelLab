fc3107752de7bf8bfca0896af89afba0a016f936
vishwesh <vishweshsoni@gmail.com>
2019-03-26 13:38:49 +0530

    mbtest

--- README.md
+++
@@ -1,4 +0,0 @@
-[![Build Status](https://travis-ci.org/vishweshsoni/LabelLab.svg?branch=master)](https://travis-ci.org/vishweshsoni/LabelLab)
-
-# LabelLab
-Machine learning supported web-base image labelling tool for researcher. 
--- test/test-pages.js
+++
@@ -1,9 +0,0 @@
-var expect  = require('chai').expect;
-var request = require('request');
-
-it('Main page content', function(done) {
-    request('https://radiant-harbor-82820.herokuapp.com/animal' , function(error, response, body) {
-        expect(body).to.equal('{"message":"hello animal"}');
-        done();
-    });
-});
